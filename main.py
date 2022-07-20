f640945f8e018f5acc1deae7049691f432fb03f4
tunji <Oladipupoolatunji272@gmail.com>
2022-07-20 05:22:14 +0100

    first_reset v2.0.1

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 ckeditor = CKEditor(app)
 Bootstrap(app)
